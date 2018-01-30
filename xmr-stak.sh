2b7722a585e7c54837a188de506934400b2aefee
Sabihul Anwary <3237639+sabichul@users.noreply.github.com>
2018-01-30 16:39:08 +0700

    Update xmr-stak.sh

--- xmr-stak.sh
+++ xmr-stak.sh
@@ -4,7 +4,7 @@ apt-get update
 apt-get install gcc-5 g++-5 
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5 
 apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev 
-git clone https://github.com/fireice-uk/xmr-stak.git 
+git clone "https://github.com/fireice-uk/xmr-stak.git" 
 mkdir xmr-stak/build 
 cd xmr-stak/build 
 cmake -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF .. 
