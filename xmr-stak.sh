f3539a3c1460047fac0001db34ee9c40e7e1a665
Sabihul Anwary <3237639+sabichul@users.noreply.github.com>
2018-01-30 13:16:31 +0700

    Create xmr-stak.sh

---
+++ xmr-stak.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+add-apt-repository ppa:ubuntu-toolchain-r/test 
+apt-get update 
+apt-get install gcc-5 g++-5 
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 60 --slave /usr/bin/g++ g++ /usr/bin/g++-5 
+apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev 
+git clone https://github.com/fireice-uk/xmr-stak.git 
+mkdir xmr-stak/build 
+cd xmr-stak/build 
+cmake -DCUDA_ENABLE=OFF -DOpenCL_ENABLE=OFF .. 
+make install 
+sysctl -w vm.nr_hugepages=128 
+cd bin 
+screen 
+./xmr-stak 
